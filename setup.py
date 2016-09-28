d0f5e6e009052620475f0d9083ad4e0dec937994
Johannes Werthebach <johannes.werthebach@tu-dortmund.de>
2016-09-28 10:19:17 +0200

    Created stepu.py to make modules installable with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "jopymods",
+    version = "0.1",
+    description = "Module with python functions for analysis",
+    url = "https://github.com/jwerthebach/python_modules",
+    author = "Johannes Werthebach",
+    author_email = "johannes.werthebach@tu-dortmund.de",
+    license = "MIT",
+    install_requires = [
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pandas",
+        "tables",
+        "tqdm",
+        "seaborn",
+        ],
+    packages = find_packages(),
+)
