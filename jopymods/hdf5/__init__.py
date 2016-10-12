59140a6347bdc64922447cb4cc721d5bec8d5eda
Johannes Werthebach <johannes.werthebach@tu-dortmund.de>
2016-10-12 12:41:33 +0200

    Added side_functions to init.py

--- jopymods/hdf5/__init__.py
+++ jopymods/hdf5/__init__.py
@@ -5,5 +5,6 @@ Collections of functions for plotting stuff mostly using matplotlib.pyplot
 """
 
 from ._i3hdf_to_df import *
+from ._side_functions import *
 
 __all__ = [_s for _s in dir() if not _s.startswith('_')]
